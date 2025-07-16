35ce002631ab03c8dd6819e1e36d3bbe21325fdc
Oleg Novak <oleg.novak@gmail.com>
2025-07-17 02:26:15 +0300

    Fix import structure and clear Python cache

--- __init__.py
+++ __init__.py
@@ -4,18 +4,7 @@ Garment-centric human generation nodes for ComfyUI using DreamFit with Flux
 Based on: https://github.com/bytedance/DreamFit
 """
 
-# Import only the node that exists
-from .nodes.dreamfit_sampler import DreamFitSampler
-
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique
-NODE_CLASS_MAPPINGS = {
-    "DreamFitSampler": DreamFitSampler,
-}
-
-# A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "DreamFitSampler": "DreamFit Sampler",
-}
+# Import node mappings from the sampler module
+from .nodes.dreamfit_sampler import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
